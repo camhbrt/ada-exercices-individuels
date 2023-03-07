5102ddb13985905574fa7cdce92fcb20235218b8
Camille Hebert <camille.hebert22@gmail.com>
2023-03-07 16:29:04 +0100

    etape 3 réalisée

--- ex3_01_mar/morse.js
+++ ex3_01_mar/morse.js
@@ -1,25 +1,19 @@
-//Etape 1
 // Étape 1
 // Dans un fichier JavaScript, commencez par écrire une fonction getLatinCharacterList. Cette fonction va prendre en paramètre du texte et retourner un tableau contenant chaque caractère.
-
 // Tester la fonction en lui donnant une chaine de caractères “Hello, world”, et assurez-vous qu’elle renvoie bien ["H", "e", "l", "l", "o", ",", " ", "w", "o", "r", "l", "d"]
 
 function getLatinCharacterList(string) { 
 	return string.split("")
-}
-//fonction toUpperCase() transforme en majuscule
-let characterArray = getLatinCharacterList("Hello, world");
-console.log(characterArray);
-
+};
+let latinArray = getLatinCharacterList("S, OS");
+console.log(latinArray);
 
 //Etape 2
 // À votre fichier, ajouter le dictionnaire en annexe 1. Il vous permettra d’établir une table de correspondance entre les caractères alphabets latin et l’alphabet morse.
-
 // Créer une fonction translateLatinCharacter qui prend un paramètre un caractère et renvoie sa correspondance en morse.
-
 // Tester la fonction en lui donnant en paramètre “A”, et assurez-vous qu’elle renvoie bien .-.
 
-const latinToMorse = {
+const latinToMorse = { 
 	'A':'.-',
 	'B':'-...',
 	'C':'-.-.',
@@ -46,16 +40,35 @@ const latinToMorse = {
 	'X':'-..-',
 	'Y':'-.--',
 	'Z':'--..'
-}
-//console.log(latinToMorse["A"])
+};
 
 function translateLatinCharacter(letter){
-    return latinToMorse[letter];
-}
-console.log(translateLatinCharacter.("B"))
+	let maj = letter.toUpperCase();
+    return latinToMorse[maj]
+};
+let codedLetter = translateLatinCharacter("f");
+console.log(`la lettre "f" en morse est `+codedLetter);
 
 
 // Étape 3
 // Ajouter une nouvelle fonction encode qui prend en paramètre du texte et qui va utiliser la fonction de l’étape 1, pour chaque caractère, appliquer la fonction de l’étape 2 et ainsi récupérer son équivalent morse.
-
 // Attention: la table de correspondance ne contient que des caractères majuscules.
+
+
+
+function encode(text){
+	//text se transforme en tableau avec chaque lettre séparée => ['T', 'E', 'X', 'T']
+	let latinArray = getLatinCharacterList(text);
+	console.log(latinArray);
+	let morseArray=[] //déclare tableau vide à remplir
+
+	//chaque lettre du tableau doit être traduite en morse
+	for (i= 0; i<latinArray.length; i++) {
+		morse = translateLatinCharacter(latinArray[i]);
+		morseArray.push(morse);
+	}
+
+	return morseArray
+};
+
+console.log(encode("camille"))
